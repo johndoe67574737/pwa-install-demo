6fd72a5f73c23b830bb29300c6af92a9c8c1f4c9
Chiaramonte, Marcello <marcello.chiaramonte@juliusbaer.com>
2022-05-10 17:35:19 +0200

    changed whole app

--- src/components/NotificationView.js
+++ src/components/NotificationView.js
@@ -9,6 +9,8 @@ class NotificationView extends React.Component {
   token = "ca6a6fa6-f403-4595-85fc-615d0d73f521";
 
   componentDidMount() {
+    document.title = "Dealer Intervention Tool";
+
     this.ws.onopen = () => {
       // on connecting, do nothing but log it to the console
       console.log("connected");
@@ -53,17 +55,54 @@ class NotificationView extends React.Component {
   notification;
 
   displayWindowsNotification() {
+    var isOldTitle = true;
+    var oldTitle = "Dealer Intervention Tool";
+    var newTitle = "--- New Order Awaits Input ---";
+    var interval = null;
+
+    function changeFavicon(src) {
+      var link = document.createElement("link"),
+        oldLink = document.getElementById("dynamic-favicon");
+      link.id = "dynamic-favicon";
+      link.rel = "shortcut icon";
+      link.href = src;
+      if (oldLink) {
+        document.head.removeChild(oldLink);
+      }
+      document.head.appendChild(link);
+    }
+
+    function changeTitle() {
+      document.title = isOldTitle ? oldTitle : newTitle;
+      isOldTitle = !isOldTitle;
+
+      changeFavicon(isOldTitle ? "" : "");
+    }
+    interval = setInterval(changeTitle, 700);
+
+    // window.onfocus(() => {
+
+    // });
+    // window.focus(function () {
+    //   clearInterval(interval);
+    //   document.title = oldTitle;
+    // });
+
     Notification.requestPermission().then(function () {});
 
-    this.notification = new Notification("Big Deal", {
+    this.notification = new Notification("Dealer Intervention", {
       body: "Click here",
-      icon: "https://en.wikipedia.org/static/images/project-logos/enwiki.png",
+      icon: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_462343.png&f=1&nofb=1",
+      image:
+        "https://images.freeimages.com/images/small-previews/88e/money-money-money-1241634.jpg",
       requireInteraction: true,
     });
+
     this.notification.addEventListener("click", function () {
       // eslint-disable-next-line no-restricted-globals
-
-      window.open("http://localhost:3000");
+      window.focus(); // open("http://localhost:3000");
+      document.title = oldTitle;
+      clearInterval(interval);
     });
   }
 
@@ -98,7 +137,7 @@ class NotificationView extends React.Component {
         >
           Windows Notification via react-desktop-notification
         </button>
-        <button
+        {/* <button
           className="sendNotification"
           onClick={() => this.sendNotification(2)}
         >
@@ -109,7 +148,7 @@ class NotificationView extends React.Component {
           onClick={() => this.displayWindowsNotification()}
         >
           Send Windows Notification locally
-        </button>
+        </button> */}
       </div>
     );
   }

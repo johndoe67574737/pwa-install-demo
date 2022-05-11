d390eb1557e67d24f6d1dcca6b1b28f541acd047
Chiaramonte, Marcello <marcello.chiaramonte@juliusbaer.com>
2022-05-11 08:29:31 +0200

    added pwa

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
 import "./App.css";
 import NotificationView from "./components/NotificationView";
 
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+let deferredPrompt;
+
 function App() {
-  return <NotificationView />;
+  const [installable, setInstallable] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI notify the user they can install the PWA
+      setInstallable(true);
+    });
+
+    window.addEventListener("appinstalled", () => {
+      // Log install to analytics
+      console.log("INSTALL: Success");
+    });
+  }, []);
+
+  const handleInstallClick = (e) => {
+    // Hide the app provided install promotion
+    setInstallable(false);
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the install prompt");
+      } else {
+        console.log("User dismissed the install prompt");
+      }
+    });
+  };
+
+  return (
+    <div className="App">
+      <header>
+        {installable && (
+          <button className="sendNotification" onClick={handleInstallClick}>
+            Install
+          </button>
+        )}
+      </header>
+      <NotificationView />
+    </div>
+  );
 }
 
 export default App;
